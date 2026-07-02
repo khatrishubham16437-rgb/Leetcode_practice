88a39856ba73b79407f9d2484a565cd96c5de129
Shubham Khatri <khatrishubham16437@gmail.com>
2026-07-02 07:16:55 +0530

    Isomorphic String

---
+++ Strings/Isomorphic String.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isomorphicString(string s, string t) {
+    	//your code goes here
+        char map_s[128] = {0};
+        char map_t[128] = {0};
+        for(int i=0 ; i<s.size() ; i++)
+        {
+            if(map_s[s[i]] != map_t[t[i]]) return false;
+            map_s[s[i]] = i+1;
+            map_t[t[i]] = i+1;
+        }
+        return true;
+    }
+};
